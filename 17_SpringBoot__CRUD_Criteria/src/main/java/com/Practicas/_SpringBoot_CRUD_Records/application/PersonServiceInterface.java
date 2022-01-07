363f03bee49270e7899339d63c8393ca8ff2fef9
Daniel Del Río Montes <daniel.delriomontes@bosonit.com>
2022-01-07 10:42:08 +0100

    primera parte

---
+++ 17_SpringBoot__CRUD_Criteria/src/main/java/com/Practicas/_SpringBoot_CRUD_Records/application/PersonServiceInterface.java
@@ -0,0 +1,25 @@
+package com.Practicas._SpringBoot_CRUD_Records.application;
+
+import com.Practicas._SpringBoot_CRUD_Records.application.DTOs.PersonaDTOinput;
+import com.Practicas._SpringBoot_CRUD_Records.application.DTOs.PersonaDTOoutput;
+import com.Practicas._SpringBoot_CRUD_Records.domain.Person;
+
+import java.util.Date;
+import java.util.List;
+
+public interface PersonServiceInterface {
+
+    PersonaDTOoutput addPerson(PersonaDTOinput personaDTOinput)throws Exception;
+
+    List<PersonaDTOoutput> getPersonsByName(String name)throws Exception;
+
+    List<PersonaDTOoutput> getAll()throws Exception;
+
+    PersonaDTOoutput getPersonById(long id)throws Exception;
+
+    PersonaDTOoutput updatePersonById(long id, PersonaDTOinput personaDTOinput)throws Exception;
+
+    List<PersonaDTOoutput> deleteById(long id)throws Exception;
+
+    List<PersonaDTOoutput> getByCriteria(String user, String name, String surname, Date createdDate, String dateCondition)throws Exception;
+}
---
+++ 17_SpringBoot__CRUD_Criteria/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO PERSONAS (id,usuario,password,name,surname,company_email,personal_email,city,active,created_date) VALUES (1,'juan123','cont12345','JuanJose', 'Rodríguez', 'juanjo@com.com', 'juanjo@blabla.com', 'Caceres', true, {d '2018-07-24'});
+
