de4eb5d3c492bc7693b17a6cc63141850fb3f9a3
Daniel Del Río Montes <daniel.delriomontes@bosonit.com>
2022-02-11 11:17:40 +0100

    only upload one file

---
+++ 19_SpringBoot_SA2_Ficheros/src/main/java/_SpringBoot_SA2_Ficheros/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package _SpringBoot_SA2_Ficheros.controllers;
+
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MultipartException.class)
+    public String handleMultipartException(MultipartException e, RedirectAttributes attributes) {
+        attributes.addFlashAttribute("message", e.getCause().getMessage());
+
+        return "redirect:/status";
+    }
+
+}
